75d3ef3e1e44d7cccb20142590b180867fe44e48
JamieH-alt <jamie.hanson@education.nsw.gov.au>
2024-08-13 15:06:51 +1000

    Finished GUI
    Fixed the problem with the GUI where elements would dupliact when a text element exists.
    
    This was a problem as the number of rawentrycount was doubling up when it came to detailedEntry's.
    
    Also added a minus button to remove entries. Made it so it sorts options for aesthetic purposes.

--- code/project_ui.py
+++ code/project_ui.py
@@ -26,33 +26,82 @@ frame2.columnconfigure(1, weight=1)
 frame2.columnconfigure(2, weight=1)
 
 label = customtkinter.CTkLabel(master=frame2, text="Temperature Mapping", font=("Roboto", 18))
-label.place(relx=0.05, rely=0)
 
+entryvalues = []
 entries = []
 entrycolumn = -1
 entryrowstart = 0
 entryrow = 0
+rawentrycount = 0
+entrycount = 0
+
+def remove(entry: customtkinter.CTkEntry, button: customtkinter.CTkButton):
+    global entrycount, rawentrycount, entryvalues
+    global frame
+    if (entrycount == 1):
+        return
+    for i in frame.winfo_children():
+        if (isinstance(i, customtkinter.CTkEntry)):
+            if (i.get() != "" and i.get() != entry.get()):
+                entryvalues.append(i.get())             
+    entry.destroy()
+    button.destroy()
+    entrycount = 0
+    rawentrycount -= 1
+    for i in frame.winfo_children():
+        i.destroy()
+    for i in entryvalues:
+        addDetailedEntry(i)
+        rawentrycount -= 1
+    rawentrycount -= len(entryvalues)
+    entryvalues = []
+    for i in range(0, rawentrycount):
+        addEntry()
+    rawentrycount = entrycount
+
+def addDetailedEntry(string):
+    global frame
+    global entrycolumn
+    global entryrow, entryrowstart
+    global entrycount, rawentrycount
+    if (entrycount >= 36):
+        return
+    if entrycount % 12 == 0:
+        entrycolumn = entrycount/12
+    stringvar = customtkinter.StringVar()
+    stringvar.set(string)
+    entryrow = entrycount % 12
+    entry = customtkinter.CTkEntry(master=frame, placeholder_text="Location", textvariable=stringvar, width=(425/3-(340/12)))
+    entry.place(x=entrycolumn*(425/3),y=entryrow*(340/12))
+    entrybutton = customtkinter.CTkButton(master=frame, text="-", font=("Roboto", 13, "bold"), command=lambda: remove(entry, entrybutton), width=340/12, fg_color="#b94040")
+    entrybutton.place(x=(entrycolumn + 1)*(425/3) - (340/12), y=entryrow*(340/12))
+    entries.append(entry)
+    entrycount += 1
+    rawentrycount += 1
+
+
 def addEntry():
     global frame
     global entrycolumn
     global entryrow, entryrowstart
-    if entrycolumn == 2:
-        if (len(entries) % 12 == 0):
-            return
-    if len(entries) % 12 == 0:
-        if (entrycolumn == 2):
-            entrycolumn = 0
-        else:
-            entrycolumn += 1
-        entryrow = entryrowstart
-    entryrow += 1
-    entry = customtkinter.CTkEntry(master=frame, placeholder_text="Location").grid(row=entryrow, column=entrycolumn)
+    global entrycount, rawentrycount
+    if (entrycount >= 36):
+        return
+    if entrycount % 12 == 0:
+        entrycolumn = entrycount/12
+    entryrow = entrycount % 12
+    entry = customtkinter.CTkEntry(master=frame, placeholder_text="Location", width=(425/3-(340/12)))
+    entry.place(x=entrycolumn*(425/3),y=entryrow*(340/12))
+    entrybutton = customtkinter.CTkButton(master=frame, text="-", font=("Roboto", 13, "bold"), command=lambda: remove(entry, entrybutton), width=340/12, fg_color="#b94040")
+    entrybutton.place(x=(entrycolumn + 1)*(425/3) - (340/12), y=entryrow*(340/12))
     entries.append(entry)
+    entrycount += 1
+    rawentrycount += 1
 
 def enterButton():
     locations = []
     for i in frame.winfo_children():
-        if (i.get() != ""):
+        if (isinstance(i, customtkinter.CTkEntry) and i.get() != ""):
             locations.append(i.get())
     if len(locations) == 0:
         return
